2dd95c2292b3f522cafdde0aed420c78c298a1a5
Michael Bebenita <mbebenita@mozilla.com>
2012-05-11 14:43:20 -0700

    Update Pages.

--- jc/jc.js
+++ jc/jc.js
@@ -1,41 +1,31 @@
 load("util.js");
 var options = new OptionSet("option(s)");
+var help = options.register(new Option("help", "h", false, "Print compiler options."));
+// var output = options.register(new Option("o", "o", false, "Output file name."));
 
 load("esprima.js");
 load("escodegen.js");
 load("compiler.js");
 
-
-if (arguments.length === 0) {
-  printUsage();
-  quit();
-}
-
 function printUsage() {
-  print("avm: [option(s)] file");
+  print("*JS Compiler: [option(s)] file");
+  options.trace(new IndentingWriter());
 }
 
-var file = arguments[arguments.length - 1];
-options.parse(arguments.slice(0, arguments.length - 1));
+if (arguments.length > 0) {
+  options.parse(arguments.slice(0, arguments.length - 1));
+}
 
-var output = options.register(new Option("o", "o", false, "Output"));
-
-if (help.value) {
+if (arguments.length === 0 || help.value) {
   printUsage();
   quit();
 }
 
 var file = arguments[arguments.length - 1];
-options.parse(arguments.slice(0, arguments.length - 1));
-
-// print (snarf(file));
-
 var source = snarf(file);
-// var node = esprima.parse(source);
-var node = esprima.parse(source, {loc: true});
 
+var node = esprima.parse(source, {loc: true});
 var name = file.substr(0, file.lastIndexOf('.')) || file;
 
 node = compile(node, name);
-
 print (escodegen.generate(node, {base: "", indent: "  "}));
