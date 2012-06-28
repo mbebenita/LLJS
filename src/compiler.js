2f44101cfadefc7d0b3b20ee0a9b6011b07a5007
Stephen Crane <scrane@mozilla.com>
2012-06-28 15:36:19 -0700

    Allow function assignment of functions with different number of args. Checks to make sure that the different args are dyn and at the end of the argument list.

--- src/compiler.js
+++ src/compiler.js
@@ -824,12 +824,31 @@
 
     var paramTypes = this.paramTypes;
     var otherParamTypes = other.paramTypes;
-    if (otherParamTypes.length != paramTypes.length) {
-      return false;
-    }
 
     for (var i = 0, j = paramTypes.length; i < j; i++) {
-      if (!paramTypes[i].assignableFrom(otherParamTypes[i])) {
+      if (otherParamTypes.length <= i) {
+        if (paramTypes[i] !== undefined) {
+          // Other arrow has too few params, and this param isn't dyn
+          return false;
+        } else {
+          continue;
+        }
+      }
+
+      if (paramTypes[i] === undefined) {
+        if (otherParamTypes[i] !== undefined) {
+          return false;
+        }
+      } else {
+        if (!paramTypes[i].assignableFrom(otherParamTypes[i])) {
+          return false;
+        }
+      }
+    }
+
+    for (var i = paramTypes.length, j = otherParamTypes.length; i < j; i++) {
+      if (otherParamTypes[i] !== undefined) {
+        // Other arrow has more typed params
         return false;
       }
     }
