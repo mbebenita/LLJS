f7fb2813537137fbddcbb27ce541282aa3743e3c
Tim Disney <tim.disney@gmail.com>
2012-07-12 16:23:29 -0700

    new command format

--- tests.sh
+++ tests.sh
@@ -11,35 +11,22 @@ mkdir -p build/node
 mkdir -p build/sm
 
 # build for node
-bin/ljc -0 src/memory.ljs
-bin/ljc -0 src/memcheck.ljs
-bin/ljc -m -0 src/tests/test-memcheck.ljs
-# move to node builddir
-cp src/memory.js build/node
-cp src/memcheck.js build/node
-cp src/tests/test-memcheck.js build/node
+bin/ljc -0 -o build/node/memory.js src/memory.ljs
+bin/ljc -0 -o build/node/memcheck.js src/memcheck.ljs
+bin/ljc -m -0 -o build/node/test-memcheck.js src/tests/test-memcheck.ljs
 
 # build for sm
-bin/ljc -l -0 src/memory.ljs
-bin/ljc -l -0 src/memcheck.ljs
-bin/ljc -l -m -0 src/tests/test-memcheck.ljs
-# move to sm builddir
-cp src/memory.js build/sm
-cp src/memcheck.js build/sm
-cp src/tests/test-memcheck.js build/sm
+bin/ljc -l -0 -o build/sm/memory.js src/memory.ljs
+bin/ljc -l -0 -o build/sm/memcheck.js src/memcheck.ljs
+bin/ljc -l -m -0 -o build/sm/test-memcheck.js src/tests/test-memcheck.ljs
 
 # build bench for sm (no memcheck)
-bin/ljc -l -0 benchmarks/linked-list.ljs
-bin/ljc -l -0 benchmarks/access-nbody.ljs
-# move bench to sm builddir
-cp benchmarks/access-nbody.js build/sm/access-nbody.js
-cp benchmarks/linked-list.js build/sm/linked-list.js
+bin/ljc -l -0 -o build/sm/linked-list.js benchmarks/linked-list.ljs
+bin/ljc -l -0 -o build/sm/access-nbody.js benchmarks/access-nbody.ljs
 
 # build bench for sm (with memcheck)
-bin/ljc -l -m -0 benchmarks/access-nbody.ljs
-bin/ljc -l -m -0 benchmarks/linked-list.ljs
-cp benchmarks/access-nbody.js build/sm/access-nbody-memcheck.js
-cp benchmarks/linked-list.js build/sm/linked-list-memcheck.js
+bin/ljc -l -m -0 -o build/sm/access-nbody-memcheck.js benchmarks/access-nbody.ljs
+bin/ljc -l -m -0 -o build/sm/linked-list-memcheck.js benchmarks/linked-list.ljs
 
 cp src/memcheck-harness.js build/sm
 
