dbdb7795686230c67fa7816e19fe19b676f57c15
Stephen Crane <scrane@mozilla.com>
2012-07-16 16:37:22 -0700

    Fixing loading of memory.js

--- src/compiler.js
+++ src/compiler.js
@@ -1728,7 +1728,7 @@
    */
 
   function createRequire(name) {
-    return new CallExpression(new Identifier("require"), [new Literal(name)]);
+    return new CallExpression(new Identifier("require"), [new Literal("./" + name)]);
   }
 
   function createModule(program, name, bare, loadInstead) {
